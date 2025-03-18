5884f200885614cedd2c659bc9a3a07ad0e6f612
Gabriel Reimberg <86269694+ArticN@users.noreply.github.com>
2025-03-18 10:53:06 -0300

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,1 +1,134 @@
+#include <stdlib.h>
+#include <omp.h>
+#include <stdio.h>
+#include <time.h>
+#define SIZE 10000
 
+void printV(int v[]){
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", v[i]);
+    printf("\n");
+}
+
+void merge(int v[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *LVec = (int*) malloc(n1 * sizeof(int));
+    int *RVec = (int*) malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        LVec[i] = v[l + i];
+    for (j = 0; j < n2; j++)
+        RVec[j] = v[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (LVec[i] <= RVec[j]) {
+            v[k] = LVec[i];
+            i++;
+        }
+        else {
+            v[k] = RVec[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        v[k] = LVec[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        v[k] = RVec[j];
+        j++;
+        k++;
+    }
+
+    free(LVec);
+    free(RVec);
+}
+
+void merge_sort(int v[], int l, int r){
+    if(l < r){
+        int m = l + (r - l) / 2;
+
+        merge_sort(v, l, m);
+        merge_sort(v, m+1, r);
+
+        merge(v, l, m, r);
+    }
+}
+
+void merge_sort_iterative(int v[], int n) {
+    int act;
+    int l;
+
+    for (act = 1; act <= n-1; act = 2*act) {
+        #pragma omp parallel for private(l) shared(v)
+        for (l = 0; l < n-1; l += 2*act) {
+            int m = l + act - 1;
+            int r = (l + 2*act - 1 < n-1) ? l + 2*act - 1 : n-1;
+
+            merge(v, l, m, r);
+        }
+    }
+}
+
+void run_and_measure(void (*sort_func)(int[], int, int), int v[], int n, double *times) {
+    clock_t start, end;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < n; j++) {
+            v[j] = rand() % 100;
+        }
+        start = clock();
+        sort_func(v, 0, n - 1);
+        end = clock();
+        times[i] = ((double) (end - start)) / CLOCKS_PER_SEC;
+    }
+}
+
+void run_and_measure_iterative(void (*sort_func)(int[], int), int v[], int n, double *times) {
+    clock_t start, end;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < n; j++) {
+            v[j] = rand() % 100;
+        }
+        start = clock();
+        sort_func(v, n);
+        end = clock();
+        times[i] = ((double) (end - start)) / CLOCKS_PER_SEC;
+    }
+}
+
+void print_times(double times[], int n) {
+    double sum = 0;
+    printf("Tempos de execução:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Execução %d: %f segundos\n", i + 1, times[i]);
+        sum += times[i];
+    }
+    printf("Média: %f segundos\n", sum / n);
+}
+
+int main(void){
+    srand(time(NULL));
+    int v[SIZE];
+    double times_recursive[10];
+    double times_iterative[10];
+
+    printf("Merge Sort Recursivo:\n");
+    run_and_measure(merge_sort, v, SIZE, times_recursive);
+    print_times(times_recursive, 10);
+
+    printf("\nMerge Sort Iterativo:\n");
+    run_and_measure_iterative(merge_sort_iterative, v, SIZE, times_iterative);
+    print_times(times_iterative, 10);
+
+    return 0;
+}
